d965a9cb6d1cfb42618a616d65e888a35c3a11fe
Papawy <contactpapawy@gmail.com>
2020-04-07 03:18:35 +0200

    utils: add passwordhasher and chat utils commands

---
+++ SemiRP/Utils/Chat.cs
@@ -0,0 +1,32 @@
+﻿using SampSharp.GameMode.SAMP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Utils
+{
+    class Chat
+    {
+        public static void SendMeChat(Player from, string message)
+        {
+            foreach (Player p in Player.All)
+            {
+                if (from.Position.DistanceTo(p.Position) <= Commands.ChatCommands.PROXIMITY_RADIUS)
+                {
+                    p.SendClientMessage(Color.HotPink, "* " + from.Name + " " + message);
+                }
+            }
+        }
+
+        public static void SendDoChat(Player from, string message)
+        {
+            foreach (Player p in Player.All)
+            {
+                if (from.Position.DistanceTo(p.Position) <= Commands.ChatCommands.PROXIMITY_RADIUS)
+                {
+                    p.SendClientMessage(Color.HotPink, "* (" + from.Name + ") " + message);
+                }
+            }
+        }
+    }
+}
