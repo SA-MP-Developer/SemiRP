9147089a779da8e800d19b25bf7a513305ed9991
Victor <vic-du-68@protonmail.com>
2020-04-11 21:56:25 +0200

    cmds: Add AdminCommands and PhoneCommands

---
+++ SemiRP/Commands/AdminCommands.cs
@@ -0,0 +1,19 @@
+﻿using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.SAMP.Commands;
+using SemiRP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Commands
+{
+    [CommandGroup("a", "admin")]
+    class AdminCommands
+    {
+        [Command("help", "h")]
+        private static void Help(Player sender)
+        {
+            Utils.Chat.AdminChat(sender, "help");
+        }
+    }
+}
---
+++ SemiRP/Commands/PhoneCommands.cs
@@ -0,0 +1,38 @@
+﻿using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.SAMP.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Commands
+{
+    [CommandGroup("t", "phone", "telephone")]
+    class PhoneCommands
+    {
+        [Command("sms")]
+        private static void SendSMS(Player sender, Player receiver, string message)
+        {
+            if (!receiver.IsConnected)
+            {
+                sender.SendClientMessage(Color.White, "[" + Color.DarkRed + "ERREUR" + Color.White + "] le joueur id " + receiver.Id + " n'est pas connecté.");
+                return;
+            }
+
+            if (!receiver.AcceptMP)
+            {
+                sender.SendClientMessage(Color.White, "[" + Color.DarkRed + "ERREUR" + Color.White + "] " + receiver.Name + " n'accepte pas d'SMS.");
+                return;
+            }
+
+            sender.SendClientMessage(Color.Yellow, "[SMS] " + sender.Name + " [" + sender.Id + "] : " + message);
+            receiver.SendClientMessage(Color.Yellow, "[SMS] " + sender.Name + " [" + sender.Id + "] : " + message);
+        }
+
+        [Command("sms")]
+        private static void Call(Player sender, string number)
+        {
+
+            
+        }
+    }
+}
