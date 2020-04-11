3416739164c296f6579f87be236a96908903ab7c
Victor <vic-du-68@protonmail.com>
2020-04-11 21:56:07 +0200

    utils: Add new admin function

---
+++ SemiRP/Utils/Admin.cs
@@ -0,0 +1,22 @@
+﻿using SemiRP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Utils
+{
+    class Admin
+    {
+        private static bool CheckAdmin(Player player)
+        {
+            foreach (Permission p in player.AccountData.Perms)
+            {
+                if (p.Name == "admin")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
