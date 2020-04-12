22454fea3f3bcb3582d99dd5d511f74930b32087
Papawy <contactpapawy@gmail.com>
2020-04-13 01:44:32 +0200

    utils: add SexUtils

---
+++ SemiRP/Utils/SexUtils.cs
@@ -0,0 +1,25 @@
+﻿using SemiRP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Utils
+{
+    public static class SexUtils
+    {
+        public static string SexToString(Character.CharSex sex)
+        {
+            switch (sex)
+            {
+                case Character.CharSex.NOT_SET:
+                    return "Non défini";
+                case Character.CharSex.MAN:
+                    return "Homme";
+                case Character.CharSex.WOMAN:
+                    return "Femme";
+                default:
+                    return "Non défini";
+            }
+        }
+    }
+}
