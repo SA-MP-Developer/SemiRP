bdfabb1858137f238e04528a445d55ca8edac423
Victor <vic-du-68@protonmail.com>
2020-04-13 16:20:56 +0200

    utils: Create PermissionsChecker

---
+++ SemiRP/Utils/PermissionsChecker.cs
@@ -0,0 +1,57 @@
+﻿using SemiRP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Utils
+{
+    class PermissionsChecker
+    {
+        public static bool IsAdmin(Player player)
+        {
+            foreach (Permission p in player.AccountData.Perms)
+            {
+                if (p.Name == "admin")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsGroupOwner(Player player, Group group)
+        {
+            foreach (Group g in player.ActiveCharacter.GroupOwner)
+            {
+                if (g == group)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsGroupMember(Player player, Group group)
+        {
+            foreach (GroupRank g in player.ActiveCharacter.GroupRanks)
+            {
+                if (g.ParentGroup == group)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsGroupRank(Player player, GroupRank groupRank)
+        {
+            foreach (GroupRank g in player.ActiveCharacter.GroupRanks)
+            {
+                if (g == groupRank)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
