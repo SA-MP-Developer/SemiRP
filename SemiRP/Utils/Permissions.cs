6fb98ff2aae38eea6e2d76df8e85f70fb58c752f
Papawy <contactpapawy@gmail.com>
2020-04-14 02:19:18 +0200

    utils: add permissions helper and refactor permission checkers

---
+++ SemiRP/Utils/Permissions.cs
@@ -0,0 +1,100 @@
+﻿using SemiRP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+
+namespace SemiRP.Utils
+{
+    class Permissions
+    {
+        public static bool AddAccountPerm(Account account, string permname)
+        {
+            using (var db = new ServerDbContext())
+            {
+                db.Accounts.Attach(account);
+
+                if (account.Perms.Any(p => p.Name == permname))
+                    return false; ;
+
+                account.Perms.Add(new Permission(permname));
+
+                db.SaveChanges();
+            }
+
+            return true;
+        }
+
+        public static bool RemoveAccountPerm(Account account, string permname, bool first=true)
+        {
+            using (var db = new ServerDbContext())
+            {
+                db.Accounts.Attach(account);
+
+                if (!account.Perms.Any(p => p.Name == permname))
+                    return false;
+
+                var perm = account.Perms.Where(p => p.Name == permname).First();
+
+                if (perm.ChildPermissions != null)
+                {
+                    foreach (Permission child in perm.ChildPermissions)
+                    {
+                        RemoveAccountPerm(account, child.Name, false);
+                    }
+                }
+
+                account.Perms.Remove(perm);
+                db.Permissions.Remove(perm);
+
+                if (first)
+                    db.SaveChanges();
+            }
+
+            return true;
+        }
+
+        public static List<string> ListAccountPerms(Account account)
+        {
+            List<string> result = new List<string>();
+            using (var db = new ServerDbContext())
+            {
+                db.Accounts.Attach(account);
+
+                foreach (Permission perm in account.Perms)
+                {
+                    result.AddRange(ListAccountPermChildren(account, perm.Name));
+                }
+            }
+
+            return result;
+        }
+
+        public static List<string> ListAccountPermChildren(Account account, string permname)
+        {
+            List<string> result = new List<string>();
+            using (var db = new ServerDbContext())
+            {
+                db.Accounts.Attach(account);
+
+                if (!account.Perms.Any(p => p.Name == permname))
+                    return result;
+
+                var perm = account.Perms.Where(p => p.Name == permname).First();
+
+                result.Add(perm.ParentPermission == null ? permname : perm.ParentPermission.Name + "/" + permname);
+
+                if (perm.ChildPermissions != null)
+                {
+                    foreach (Permission child in perm.ChildPermissions)
+                    {
+                        result.AddRange(ListAccountPermChildren(account, child.Name));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- SemiRP/Utils/PermissionsChecker.cs
+++ SemiRP/Utils/PermissionsChecker.cs
@@ -1,23 +1,29 @@
-﻿using SemiRP.Models;
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using SemiRP.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SemiRP.Utils
 {
     class PermissionsChecker
     {
+        public static bool IsServerOwner(Player player)
+        {
+            return player.AccountData.Perms.Any(p => p.Name == "serverowner");
+        }
+
         public static bool IsAdmin(Player player)
         {
-            foreach (Permission p in player.AccountData.Perms)
-            {
-                if (p.Name == "admin")
-                {
-                    return true;
-                }
-            }
-            return false;
+            return player.AccountData.Perms.Any(p => p.Name == "admin");
         }
+
+        public static bool IsModerator(Player player)
+        {
+            return player.AccountData.Perms.Any(p => p.Name == "modo");
+        }
+
         public static bool IsGroupOwner(Player player, Group group)
         {
             foreach (Group g in player.ActiveCharacter.GroupOwner)
