bd5e1ca7249ca2ed48cf93687a19dd891b561d63
Victor <vic-du-68@protonmail.com>
2020-04-15 18:47:16 +0200

    cmds: Adding Call commands

--- SemiRP/Commands/PhoneCommands.cs
+++ SemiRP/Commands/PhoneCommands.cs
@@ -26,15 +26,51 @@ namespace SemiRP.Commands
             Character character = Utils.ItemUtils.PhoneHelper.GetPhoneOwner(phoneReceiver);
             Player receiver = PlayerHelper.SearchCharacter(character);
             Phone phoneSender = ContainerHelper.CheckPlayerPhone(sender);
+            if (!receiver.IsConnected)
+            {
+                sender.SendClientMessage(Color.White, "[" + Color.DarkRed + "ERREUR" + Color.White + "] le joueur n'est pas connecté.");
+                return;
+            }
             sender.SendClientMessage(Color.Yellow, "[SMS] Message envoyé à "+number+" : " + message);
             receiver.SendClientMessage(Color.Yellow, "[SMS] Message reçu de" + phoneSender.Number + " : " + message);
         }
 
-        [Command("sms")]
+        [Command("appel","appeler")]
         private static void Call(Player sender, string number)
         {
-
-            
+            Phone phoneReceiver = Utils.ItemUtils.PhoneHelper.GetPhoneByNumber(number);
+            if (phoneReceiver is null)
+            {
+                sender.SendClientMessage(Color.White, "[" + Color.DarkRed + "ERREUR" + Color.White + "] le numéro " + number + " n'est pas attribué.");
+                return;
+            }
+            Character character = Utils.ItemUtils.PhoneHelper.GetPhoneOwner(phoneReceiver);
+            Player receiver = PlayerHelper.SearchCharacter(character);
+            Phone phoneSender = ContainerHelper.CheckPlayerPhone(sender);
+            if (!receiver.IsConnected)
+            {
+                sender.SendClientMessage(Color.White, "[" + Color.DarkRed + "ERREUR" + Color.White + "] le joueur n'est pas connecté.");
+                return;
+            }
+            sender.SendClientMessage(Color.White, "[" + Color.DarkRed + "APPEL" + Color.White + "] Appel en cours...");
+            receiver.SendClientMessage(Color.White, "[" + Color.DarkRed + "APPEL" + Color.White + "] Quelqu'un essaie de vous appeler ! /t dec pour décrocher.");
+            phoneReceiver.IsRinging = true;
+            var timer = new Timer(5000, true);
+            int nbr=0;
+            timer.Tick += (sender, e) => {
+                if(nbr < 3)
+                {
+                    receiver.SendClientMessage(Color.White, "[" + Color.DarkRed + "APPEL" + Color.White + "] Quelqu'un essaie de vous appeler ! /t dec pour décrocher.");
+                    nbr++;
+                }
+                else
+                {
+                    timer.Dispose();
+                    nbr = 0;
+                    phoneReceiver.IsRinging = false;
+                }
+                
+            };
         }
     }
 }
--- SemiRP/Models/ItemHeritage/Phone.cs
+++ SemiRP/Models/ItemHeritage/Phone.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace SemiRP.Models.ItemHeritage
@@ -8,13 +9,17 @@ namespace SemiRP.Models.ItemHeritage
     {
         private string number;
         private bool defaultPhone;
-        public Phone(string number, bool defaultPhone)
+        private bool isRinging;
+        public Phone(string number, bool defaultPhone, bool isRinging)
         {
             this.Number = number;
             this.DefaultPhone = defaultPhone;
+            this.IsRinging = isRinging;
         }
 
         public string Number { get => number; set => number = value; }
         public bool DefaultPhone { get => defaultPhone; set => defaultPhone = value; }
+        [NotMapped]
+        public bool IsRinging { get => isRinging; set => isRinging = value; }
     }
 }
