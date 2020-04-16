5ceab59b975c5beb47925c52a258f3e6b67fc232
Victor <vic-du-68@protonmail.com>
2020-04-16 23:39:13 +0200

    utils: Add InventoryHelper and GetInventory

---
+++ SemiRP/Utils/ContainerUtils/InventoryHelper.cs
@@ -0,0 +1,21 @@
+﻿using SemiRP.Models;
+using SemiRP.Models.ContainerHeritage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SemiRP.Utils.ContainerUtils
+{
+    public class InventoryHelper
+    {
+        public static Inventory GetInventory(Character character)
+        {
+            using(var db = new ServerDbContext())
+            {
+                return db.Characters.Select(x => x).Where(x => x == character).FirstOrDefault().Inventory;
+            }
+        }
+        
+    }
+}
