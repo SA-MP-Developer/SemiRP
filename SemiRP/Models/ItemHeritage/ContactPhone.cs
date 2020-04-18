79fdbda778d593bb4772445defa9f83246db4802
Victor <vic-du-68@protonmail.com>
2020-04-19 00:11:20 +0200

    data: Create ContactPhone

---
+++ SemiRP/Models/ItemHeritage/ContactPhone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Models.ItemHeritage
+{
+    public class ContactPhone
+    {
+        public ContactPhone(string name, string number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+
+        public String name { get; set; }
+        public String number { get; set; }
+    }
+}
