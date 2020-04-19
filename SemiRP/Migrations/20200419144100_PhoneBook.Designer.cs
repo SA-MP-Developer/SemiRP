ec63c56833288502f50ed556f36f6a773aa7ac10
Victor <vic-du-68@protonmail.com>
2020-04-19 17:59:54 +0200

    migrations: Add PhoneBook

--- SemiRP/Migrations/20200419144100_PhoneBook.Designer.cs
+++ SemiRP/Migrations/20200419144100_PhoneBook.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SemiRP;
 
 namespace SemiRP.Migrations
 {
     [DbContext(typeof(ServerDbContext))]
-    partial class ServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200419144100_PhoneBook")]
+    partial class PhoneBook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SemiRP/Migrations/20200419144100_PhoneBook.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SemiRP.Migrations
+{
+    public partial class PhoneBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxContact",
+                table: "Items",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ContactPhone",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Number = table.Column<string>(nullable: true),
+                    PhoneId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactPhone", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContactPhone_Items_PhoneId",
+                        column: x => x.PhoneId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactPhone_PhoneId",
+                table: "ContactPhone",
+                column: "PhoneId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactPhone");
+
+            migrationBuilder.DropColumn(
+                name: "MaxContact",
+                table: "Items");
+        }
+    }
+}
