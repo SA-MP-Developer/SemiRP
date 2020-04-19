550f372be34e01d8b2fbccd226e84b931cab5424
Victor <vic-du-68@protonmail.com>
2020-04-19 23:00:56 +0200

    cmds: Add new commands to Inventory

--- SemiRP/Commands/InventoryCommands.cs
+++ SemiRP/Commands/InventoryCommands.cs
@@ -1,4 +1,7 @@
 ﻿using SampSharp.GameMode.SAMP.Commands;
+using SemiRP.Models;
+using SemiRP.Utils.ContainerUtils;
+using SemiRP.Utils.ItemUtils;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +15,30 @@ namespace SemiRP.Commands
         [Command("mettre")]
         private static void PutItemInInventory (Player player){
             
+            try
+            {
+                Item item = ItemHelper.GetNearestItemOfCharacter(player.ActiveCharacter);
+                ItemHelper.ItemIsCloseEnoughOfPlayer(player, item);
+                InventoryHelper.AddItemToCharacter(player.ActiveCharacter, item);
+                Utils.Chat.InfoChat(player, "L'objet a été ajouté à l'inventaire.");
+            }
+            catch(Exception e)
+            {
+                Utils.Chat.ErrorChat(player, "L'ajout de l'objet dans l'inventaire à échoué : "+e.Message);
+            }
+        }
+
+        [Command("prendre")]
+        private static void GetItemFromInventory(Player player, int slotNumber)
+        {
+            try
+            {
+
+            }
+            catch(Exception e)
+            {
+                Utils.Chat.ErrorChat(player, "Impossible de prendre l'objet depuis l'inventaire : " + e.Message);
+            }
         }
     }
 }
