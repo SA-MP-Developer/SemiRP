60b6d5f28043cf7563174191f6e0512e6d930f52
Victor <vic-du-68@protonmail.com>
2020-04-19 22:59:49 +0200

    data: Add Position to item model

--- SemiRP/Models/Item.cs
+++ SemiRP/Models/Item.cs
@@ -12,17 +12,20 @@ namespace SemiRP.Models
         private double weight;
         private int quantity;
         private Container currentContainer;
+        private SpawnLocation spawnLocation;
+
         public Item()
         {
         }
 
-        public Item(int id, string name, double weight, int quantity, Container currentContainer)
+        public Item(int id, string name, double weight, int quantity, Container currentContainer, SpawnLocation spawnLocation)
         {
             this.Id = id;
             this.Name = name;
             this.Weight = weight;
             this.Quantity = quantity;
             this.currentContainer = currentContainer;
+            this.spawnLocation = spawnLocation;
         }
 
         [Key]
@@ -31,5 +34,6 @@ namespace SemiRP.Models
         public double Weight { get => weight; set => weight = value; }
         public int Quantity { get => quantity; set => quantity = value; }
         public virtual Container CurrentContainer { get => currentContainer; set => currentContainer = value; }
+        public virtual SpawnLocation SpawnLocation { get => spawnLocation; set => spawnLocation = value; }
     }
 }
