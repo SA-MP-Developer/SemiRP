9e9a7b6e637c46794f4bb030e2f1ccd11b9d342a
Victor <vic-du-68@protonmail.com>
2020-04-23 23:11:40 +0200

    data: Edit Item, remove Weight

--- SemiRP/Models/Item.cs
+++ SemiRP/Models/Item.cs
@@ -9,7 +9,6 @@ namespace SemiRP.Models
     {
         private int id;
         private String name;
-        private double weight;
         private int quantity;
         private Container currentContainer;
         private SpawnLocation spawnLocation;
@@ -18,11 +17,10 @@ namespace SemiRP.Models
         {
         }
 
-        public Item(int id, string name, double weight, int quantity, Container currentContainer, SpawnLocation spawnLocation)
+        public Item(int id, string name, int quantity, Container currentContainer, SpawnLocation spawnLocation)
         {
             this.Id = id;
             this.Name = name;
-            this.Weight = weight;
             this.Quantity = quantity;
             this.currentContainer = currentContainer;
             this.spawnLocation = spawnLocation;
@@ -31,7 +29,6 @@ namespace SemiRP.Models
         [Key]
         public int Id { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
-        public double Weight { get => weight; set => weight = value; }
         public int Quantity { get => quantity; set => quantity = value; }
         public virtual Container CurrentContainer { get => currentContainer; set => currentContainer = value; }
         public virtual SpawnLocation SpawnLocation { get => spawnLocation; set => spawnLocation = value; }
