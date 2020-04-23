6f3df1bffc527999a61b3b6df63fe3e8e7123b2b
Victor <vic-du-68@protonmail.com>
2020-04-23 23:15:09 +0200

    data: Adding new data for weapons

---
+++ SemiRP/Data/Weapons.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Data
+{
+    public static class Weapons
+    {
+        public static Dictionary<int, String> WeaponsDictionnary = new Dictionary<int, string>()
+        {
+            {0, "Poing"},
+            {1,"Poing Américain"},
+            {2, "Club de Golf"},
+            {3,"Matraque"},
+            {4,"Couteau"},
+            {5,"Batte de Baseball"},
+            {6,"Pelle"},
+            {7,"Queue de Billard"},
+            {8,"Katana"},
+            {9,"Tronçonneuse"},
+            {10,"Double Dildo"},
+            {11,"Dildo"},
+            {12,"Vibromasseur"},
+            {13,"Vibromasseur d'argent"},
+            {14,"Fleurs"},
+            {15,"Canne"},
+            {16,"Grenade"},
+            {17,"Grenade Fumigène"},
+            {18,"Cocktail Molotov"},
+            {22,"9mm"},
+            {23,"9mm Silencieux"},
+            {24,"Desert Eagle"},
+            {25,"Fusil à Pompe"},
+            {26,"Fusil à Canon Scié"},
+            {27,"Fusil de Combat"},
+            {28,"Uzi"},
+            {29,"MP5"},
+            {30,"AK-47"},
+            {31,"M4"},
+            {32,"Tec-9"},
+            {33,"Fusil de Campagne"},
+            {34,"Fusil de Sniper"},
+            {35,"RPG"},
+            {36,"HS Rocket"},
+            {37,"Lance Flamme"},
+            {38,"Minigun"},
+            {39,"Charge de C4"},
+            {40,"Détonateur"},
+            {41,"Bombe Tag"},
+            {42,"Extincteur"},
+            {43,"Camera"},
+            {44,"Lunettes à Vision Nocturne"},
+            {45,"Lunettes à Infra-rouge"},
+            {46,"Parachute"}
+        };
+    }
+}
