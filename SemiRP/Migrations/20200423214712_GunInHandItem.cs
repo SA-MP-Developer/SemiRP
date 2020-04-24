ef56e81bb4a1fd4a14616b65a4f49d2343d61b0d
Papawy <contactpapawy@gmail.com>
2020-04-24 22:12:39 +0200

    migrations: add Gun and InHandItem migrations

---
+++ SemiRP/Migrations/20200423214712_GunInHandItem.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SemiRP.Migrations
+{
+    public partial class GunInHandItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "Items");
+
+            migrationBuilder.AddColumn<int>(
+                name: "idWeapon",
+                table: "Items",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItemInHandId",
+                table: "Characters",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_ItemInHandId",
+                table: "Characters",
+                column: "ItemInHandId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Items_ItemInHandId",
+                table: "Characters",
+                column: "ItemInHandId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Items_ItemInHandId",
+                table: "Characters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_ItemInHandId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "idWeapon",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "ItemInHandId",
+                table: "Characters");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Weight",
+                table: "Items",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
