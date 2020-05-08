6cd8ab89e81d22ffd533b362d9ee629bdeee19b7
Papawy <contactpapawy@gmail.com>
2020-05-09 00:02:12 +0200

    groups: add Owner to abstract character and group

---
+++ SemiRP/Models/Owner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SemiRP.Models
+{
+    public class Owner
+    {
+        public Owner()
+        {
+            Character = null;
+            Group = null;
+        }
+
+        public Owner(Character chr)
+        {
+            Character = chr;
+        }
+
+        public Owner(Group grp)
+        {
+            Group = grp;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public virtual Character Character { get; set; }
+        public virtual Group Group { get; set; }
+    }
+}
