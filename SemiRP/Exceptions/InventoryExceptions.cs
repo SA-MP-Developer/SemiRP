207d49bba9d99e0f5ed913deb8eb4be2de682e84
Victor <vic-du-68@protonmail.com>
2020-05-09 23:31:08 +0200

    exceptions: Adding new custom exceptions for inventory

---
+++ SemiRP/Exceptions/InventoryExceptions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemiRP.Exceptions
+{
+    public class InventoryExceptions : Exception
+    {
+        public InventoryExceptions()
+        {
+
+        }
+        public InventoryExceptions(string message) : base(message)
+        {
+
+        }
+        public InventoryExceptions(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+
+    public class InventoryAddingExceptions : Exception
+    {
+        public InventoryAddingExceptions()
+        {
+
+        }
+        public InventoryAddingExceptions(string message) : base(message)
+        {
+
+        }
+        public InventoryAddingExceptions(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+
+    public class InventoryRemovingExceptions : Exception
+    {
+        public InventoryRemovingExceptions()
+        {
+
+        }
+        public InventoryRemovingExceptions(string message) : base(message)
+        {
+
+        }
+        public InventoryRemovingExceptions(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
