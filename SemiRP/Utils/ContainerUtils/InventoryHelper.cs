7cd4e52737f5400329500ebf5c76be1df6727339
Victor <vic-du-68@protonmail.com>
2020-05-09 23:32:01 +0200

    utils: Adding RemoveItemFromCharacter and fix AddItemToCharacter

--- SemiRP/Utils/ContainerUtils/InventoryHelper.cs
+++ SemiRP/Utils/ContainerUtils/InventoryHelper.cs
@@ -1,4 +1,5 @@
-﻿using SemiRP.Models;
+﻿using SemiRP.Exceptions;
+using SemiRP.Models;
 using SemiRP.Models.ContainerHeritage;
 using System;
 using System.Collections.Generic;
@@ -19,26 +20,52 @@ namespace SemiRP.Utils.ContainerUtils
             ServerDbContext dbContext = ((GameMode)GameMode.Instance).DbContext;
             return dbContext.Characters.Select(x => x).Where(x => x == character).FirstOrDefault().Inventory.ListItems.Select(x=>x).Where(x=>x.Name == name).FirstOrDefault();
         }
-        public static void AddItemToCharacter(Character character, Item item)
+        public static Item AddItemToCharacter(Character character, Item item)
         {
             ServerDbContext dbContext = ((GameMode)GameMode.Instance).DbContext;
             
             if (character.Inventory == null)
-                throw new Exception("Le joueur n'a pas d'inventaire.");
+                throw new InventoryAddingExceptions("Le joueur n'a pas d'inventaire.");
 
             if(character.ItemInHand == null)
             {
                 character.ItemInHand = item;
                 dbContext.SaveChanges();
+                return item;
             }
             else if(character.Inventory.ListItems.Count() < character.Inventory.MaxSpace)
             {
                 character.Inventory.ListItems.Add(item);
                 dbContext.SaveChanges();
+                return item;
             }
             else
             {
-                throw new Exception("L'inventaire est complet et la main n'est pas vide.");
+                throw new InventoryAddingExceptions("L'inventaire est complet et la main n'est pas vide.");
+            }
+        }
+        public static Item RemoveItemFromCharacter(Character character, Item item)
+        {
+            ServerDbContext dbContext = ((GameMode)GameMode.Instance).DbContext;
+            if (character.Inventory == null)
+                throw new InventoryRemovingExceptions("Le joueur n'a pas d'inventaire.");
+            if (item == null)
+                throw new InventoryRemovingExceptions("Un problème est survenu avec l'objet.");
+
+            if(character.ItemInHand == item)
+            {
+                character.ItemInHand = null;
+                dbContext.SaveChanges();
+                return item;
+            }
+            else if (character.Inventory.ListItems.Remove(item))
+            {
+                dbContext.SaveChanges();
+                return item;
+            }
+            else
+            {
+                throw new InventoryRemovingExceptions("L'objet n'existe pas dans l'inventaire ou la main du joueur.");
             }
         }
     }
